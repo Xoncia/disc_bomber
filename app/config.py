58e06d806814d34f210c70729e1e5f335f469e4c
Xonia <cerfnet@anche.no>
2025-01-24 16:59:32 +0000

    ok fuck git

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Config:
     """Application configuration class"""
     
     BASE_DIR = Path(__file__).resolve().parent.parent
-    SECRET_KEY = ""
+    SECRET_KEY = "LKASO92jd890HJAW(*&DHN@#)"
     SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
